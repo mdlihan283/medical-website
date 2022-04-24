4a47203d772deb890516c54a9683289e9837ac93
mdlihan <freelancerlihansocial@gmail.com>
2022-04-24 17:35:24 +0600

    massege

--- js/app.js
+++ js/app.js
@@ -8,3 +8,32 @@ window.onscroll = () => {
     navbar.classList.remove("barActive");
   }
 };
+
+function counter() {
+  let counter = 0;
+  let interval = setInterval(counting, 0.001);
+  let interval2 = setInterval(counting2, 0.001);
+  let interval3 = setInterval(counting3, 0.001);
+  function counting() {
+    document.getElementById("counting").textContent = counter;
+    counter = counter + 500000;
+    if (counter >= 509220314) {
+      clearInterval(interval);
+    }
+  }
+  function counting2() {
+    document.getElementById("counting2").textContent = counter;
+    counter = counter + 500000;
+    if (counter >= 450500862) {
+      clearInterval(interval2);
+    }
+  }
+  function counting3() {
+    document.getElementById("counting3").textContent = counter;
+    counter = counter + 500000;
+    if (counter >= 450500862) {
+      clearInterval(interval3);
+    }
+  }
+}
+counter();
